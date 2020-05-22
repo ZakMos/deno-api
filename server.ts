c9dfc604e33d13182e92b75cb2c7c9ebfb67befd
ZakMos <z.zza@hotmail.com>
2020-05-22 16:10:06 +0200

    setup server

---
+++ server.ts
@@ -0,0 +1,17 @@
+import { Application  } from 'https://deno.land/x/oak/mod.ts';
+
+import router from './routes.ts'
+
+const port = 8000
+
+const app = new Application()
+
+
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+
+console.log(`Server running on http://localhost:${port}`)
+
+await app.listen({ port })
